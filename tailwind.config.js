f51ade73174de9ec6323ec9a1da43041a5d32a81
Yağızhan Burak Yakar <yagizhan49@protonmail.com>
2022-07-27 22:54:54 +0300

    Synced with local

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  safelist: [
+    'text-5xl',
+    'text-4xl'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
